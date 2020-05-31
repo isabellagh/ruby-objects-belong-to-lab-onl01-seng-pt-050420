d6a160a9cc2e88ea6e9822d81f18f20b3ab4294b
isabella g henriques <isabellagh88@gmail.com>
2020-05-31 18:46:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,6 +7,11 @@ end
 
 class Song 
   
-  attr_accessor :title, :name 
+  attr_accessor :title, :artist 
+  
+  def initialize(tittle, artist)
+    @tittle = tittle
+    @artist = artist
+  end 
   
 end 
